79a2d7636c97de10c10d2135cb316dafad2bf80e
Ogabek1407 <137427263+Ogabek1407@users.noreply.github.com>
2023-12-13 23:01:31 +0500

    Add ApiExceptionFilterAttribute

--- NTierApplication.Web/Program.cs
+++ NTierApplication.Web/Program.cs
@@ -1,29 +1,11 @@
-using Microsoft.EntityFrameworkCore;
-using NTierApplication.Inffrastructure.Repository;
-using NTierApplication.Inffrastructure.Repository.Interface;
-using NTierApplication.Infrastructure.BdContext;
-using NTierApplication.Service;
-using NTierApplication.Service.Interface;
-using NTierApplication.Web.ActionHelpers;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers(options => options.Filters.Add<ApiExceptionFilterAttribute>());
+// Add services to the container.
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddTransient<IItemService, ItemService>();
-builder.Services.AddTransient<IItemRepository, ItemRepository>();
-builder.Services.AddTransient<IItemService, ItemService>();
-builder.Services.AddTransient<MainContext>();
-
-builder.Services.AddDbContext<MainContext>(options => {
-    options.UseSqlServer("Data Source=localhost\\MSSQLSERVER2022;User ID=sa;Password=1;Initial Catalog=NTierApplication;TrustServerCertificate=True;");
-});
-
 
 var app = builder.Build();
 
@@ -36,8 +18,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-app.MapControllers();
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
 
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
