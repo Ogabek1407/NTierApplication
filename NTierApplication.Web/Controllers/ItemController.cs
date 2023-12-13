c0b6a9c77dae930d5aeaaab978c70623775e26ce
Ogabek1407 <137427263+Ogabek1407@users.noreply.github.com>
2023-12-13 22:47:54 +0500

    Controllers Add

---
+++ NTierApplication.Web/Controllers/ItemController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NTierApplication.Service;
+using NTierApplication.Service.ViewModels;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace NTierApplication.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemService ItemService;
+
+        public ItemController(IItemService itemService)
+        {
+            ItemService = itemService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [SwaggerOperation(OperationId = "GetAll")]
+        public ICollection<ItemViewModel> GetAll()
+        {
+            return ItemService.GetItems();
+        }
+
+        [HttpPost(Name = "CreateNew")]
+        public ItemViewModel CreateNew(ItemViewModel itemViewModel)
+        {
+            ItemService.CreateNew(itemViewModel);
+            return itemViewModel;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [SwaggerOperation(OperationId = "GetById")]
+        public ItemViewModel GetById(long id)
+        {
+            return ItemService.GetById(id);
+        }
+    }
+}
